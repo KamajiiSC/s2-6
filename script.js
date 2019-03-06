9211fcf5882a5b2443178180dd6c7ffd91a0ba72
KamajiiSC <brandonlschaen@gmail.com>
2019-03-06 12:15:03 -0500

    commented code out and created new section for code

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
-//Print to console
-console.log("Hello World!");
+/***************************
+* Variables and Data Types *
+***************************/
+/*
+  //Print to console
+  console.log("Hello World!");
 
-//Set variable value
-var firstName = "John";
-console.log(firstName);
+  //Set variable value
+  var firstName = "John";
+  console.log(firstName);
 
-var lastName="Smith";
+  var lastName="Smith";
 
-//Numbers in js are floats even if they look like ints
-var age=28;
+  //Numbers in js are floats even if they look like ints
+  var age=28;
 
-var fullAge = true;
-console.log(fullAge);
+  var fullAge = true;
+  console.log(fullAge);
 
-//undefined var
-var job;
-console.log(job);
+  //undefined var
+  var job;
+  console.log(job);
 
-//Assigning job a value
-job = "Teacher";
-console.log(job);
+  //Assigning job a value
+  job = "Teacher";
+  console.log(job);
 
-//Incorrect variable name
-var 3years = 3;
-var &3years = 3;
-var john/mark = "John and Mark";
-var function= 23;
+  //Incorrect variable name
+  var 3years = 3;
+  var &3years = 3;
+  var john/mark = "John and Mark";
+  var function= 23;
+*/
+/***************************************
+* Variables mutation and type coercion *
+***************************************/
