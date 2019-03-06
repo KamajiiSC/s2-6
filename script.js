efd3e1c38658e5f8376be9e719210fc71f52cd9b
KamajiiSC <brandonlschaen@gmail.com>
2019-03-06 12:11:13 -0500

    added more spacing for readability

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 //Print to console
 console.log("Hello World!");
+
 //Set variable value
 var firstName = "John";
 console.log(firstName);
 
 var lastName="Smith";
+
 //Numbers in js are floats even if they look like ints
 var age=28;
 
 var fullAge = true;
 console.log(fullAge);
+
 //undefined var
 var job;
 console.log(job);
+
 //Assigning job a value
 job = "Teacher";
 console.log(job);
+
 //Incorrect variable name
 var 3years = 3;
 var &3years = 3;
