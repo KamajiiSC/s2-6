cf5a83d6ab909b1878a3ff862c297b9af788c821
KamajiiSC <brandonlschaen@gmail.com>
2019-03-06 12:04:02 -0500

    added fullAge and another console log

--- script.js
+++ script.js
@@ -5,4 +5,8 @@ var firstName = "John";
 console.log(firstName);
 
 var lastName="Smith";
+//Numbers in js are floats even if they look like ints
 var age=28;
+
+var fullAge = true;
+console.log(fullAge);
