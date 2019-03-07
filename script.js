fb9a76ecfd8eb8b3d8d6c91b4db9ecac9f462b8b
KamajiiSC <brandonlschaen@gmail.com>
2019-03-07 14:30:07 -0500

    created examples of multiple assignments

--- script.js
+++ script.js
@@ -97,9 +97,17 @@ var now = 2018;
 var yearJohn = 1989;
 var fullAge = 18;
 
+//Multiple operators
 var isFullAge = now - yearJohn >= fullAge;
 console.log(isFullAge);
 
+//Grouping
 var ageJohn = now - yearJohn;
 var ageMark = 35;
 var average = (ageJohn + ageMark) / 2;
+
+//Multiple assignments
+var x, y;
+//assignment is right to left
+x = y = (3 + 5) *  4 - 6;//8 * 4 - 6//32 - 6//26
+console.log(x, y);
