2c86f7213ba0f1bcb4973461e6dfed61d254381d
KamajiiSC <brandonlschaen@gmail.com>
2019-03-07 14:12:51 -0500

    added john and mark ages and compared them

--- script.js
+++ script.js
@@ -61,8 +61,10 @@ console.log(firstName +" " + lastName);
 /******************
 * Basic Operators *
 ******************/
-var now, yearJohn, yearMark
+var now, yearJohn, yearMark, ageJohn, ageMark;
 now = 2018;
+ageJohn = 28;
+ageMark = 33;
 
 //Math Operators
 yearJohn = now - 28;
@@ -76,3 +78,5 @@ console.log(now * 2);
 console.log(now / 10);
 
 //Logical operators
+var johnOlder = ageJohn < ageMark;
+console.log(johnOlder);
