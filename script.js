c5a51fd9103d1339bba9f541527e3b3aba01814a
KamajiiSC <brandonlschaen@gmail.com>
2019-03-08 14:03:22 -0500

    Created if else statement

--- script.js
+++ script.js
@@ -128,3 +128,9 @@ console.log(x);
 ***********************/
 let firstName = 'John';
 let civilStatus = 'single';
+
+if(civilStatus === 'married'){
+  console.log(firstName + ' is married!');
+} else{
+    console.log(firstName + ' will hopefully marry soon');
+}
