89c2b7f56ff5b72fddf6c4000945f0730a19f5c4
KamajiiSC <brandonlschaen@gmail.com>
2019-03-12 12:22:25 -0400

    Created condition with AND for between 13-19

--- script.js
+++ script.js
@@ -144,7 +144,7 @@ let age = 16;
  
 if (age < 13){
   console.log(firstName + ' is a boy.');
-} else if(){ 
+} else if(age > 12 && age < 20){ 
   console.log(firstName + ' is a teenager.');
 } else{
   console.log(firstName + ' is a man.');
