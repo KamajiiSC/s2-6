7f95b484d13dec376fb8d8bfd25ef306039066f0
KamajiiSC <brandonlschaen@gmail.com>
2019-03-12 12:26:47 -0400

    Changed constraint ages to make it more understandable

--- script.js
+++ script.js
@@ -140,11 +140,11 @@ if(civilStatus === 'married'){
 * Boolean Logic *
 ****************/
 let firstName = "John";
-let age = 16;
+let age = 16 ;
  
 if (age < 13){
   console.log(firstName + ' is a boy.');
-} else if(age > 12 && age < 20){ 
+} else if(age >= 13 && age <= 19){ 
   console.log(firstName + ' is a teenager.');
 } else{
   console.log(firstName + ' is a man.');
